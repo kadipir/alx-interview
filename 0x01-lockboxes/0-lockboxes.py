4ae500c2febcad26cbce5d5a7e9e8650ba761999
truth <trutherkadi@gmail.com>
2024-10-13 14:16:46 -0700

    py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,35 +1,20 @@
 #!/usr/bin/python3
 """
-Method to determine if all boxes can be opened using prototype: def canUnlockAll(boxes);
+Method to determine if all boxes can be opened
+Using prototype: def canUnlockAll(boxes)
 """
 
+
 def canUnlockAll(boxes):
-    """check if boxes can be unlocked"""
-    n = len(boxes) #number of boxes
-
-    #set to keep track of unlocked boxes
-    unlocked  = set()
-
-    #list to manage boxes to be explored
-    explore = [0]
-
-    while explore:
-        current_box = explore.pop()
-        
-        #if in  box explores skip
-        if current_box in explored:
-            continue
-
-        #mark the current_box as unlocked
-        unlocked.add(current_box)
-
-        #get the keys in current box
-        for key in boxes[current_box]:
-
-            #if the key is a valid box index and not yet in unlocked
-            if key < n and key not in unlocked:
-                explore.append(key)
-
-
-    #check if all unlocked
-    return len(unlocked) = n
+    """
+    Check if boxes can be unlocked
+    """
+    for key in range(1, len(boxes) - 1):
+        ctr = False
+        for idx in range(len(boxes)):
+            ctr = (key in boxes[idx] and key != idx)
+            if ctr:
+                break
+        if ctr is False:
+            return ctr
+    return True
